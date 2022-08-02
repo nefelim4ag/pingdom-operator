f1c468d8cceb89bc1b673c0ce7d3494453bc5b34
Timofey Titovets <timophey.tsitavets@fabros-team.com>
2022-08-03 01:48:44 +0200

    Added more meat
    * Use cachetools
    * Define K8s controller
    * Filter pingdom-operator managed ingresses
    * Added more logic to pingdom api controller

--- pingdom-operator.py
+++ pingdom-operator.py
@@ -4,32 +4,110 @@ import requests
 import json
 import os
 
-from functools import lru_cache
+from cachetools import cached, TTLCache
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
 
-class BearerAuth(requests.auth.AuthBase):
-    def __init__(self, token):
-        self.token = token
+class Kubernetes:
+    class Ingress:
+        def __init__(self, ingress):
+            self.name = ingress.metadata.name
+            self.namespace = ingress.metadata.namespace
+            self.annotations = ingress.metadata.annotations
+            if ingress.spec.tls:
+                self.https = True
+            self.hosts = []
+            for rule in ingress.spec.rules:
+                self.hosts.append(rule.host)
 
-    def __call__(self, r):
-        r.headers["authorization"] = "Bearer " + self.token
-        return r
-
-
-class Pingdom():
-    api_url = "https://api.pingdom.com/api/3.1/"
+        def integrations(self):
+            return self.annotations.get('pingdom-operator.io/integrations').split(',')
 
     def __init__(self):
-        token = os.environ.get('BEARER_TOKEN')
-        auth = BearerAuth(token)
-        self.s = requests.Session()
-        self.s.auth = BearerAuth(token)
-        self.s.headers['Content-type'] = 'application/json'
+        try:
+            config.load_incluster_config()
+        except config.ConfigException:
+            try:
+                config.load_kube_config()
+            except config.ConfigException:
+                raise Exception("Could not configure kubernetes python client")
+        self.v1 = client.NetworkingV1Api()
 
-    @lru_cache(maxsize=16, typed=False)
-    def checks(self):
-        response = self.s.get(self.api_url + 'checks', json={})
-        response.raise_for_status()
+    @cached(cache=TTLCache(maxsize=1, ttl=60))
+    def list_ingress_for_all_namespaces(self):
+        try:
+            response = self.v1.list_ingress_for_all_namespaces()
+        except ApiException as e:
+            print(e)
+            exit(1)
+
+        return response.items
+
+    def pingdom_ingresses(self):
+        ingresses_list = []
+        for ingress in self.list_ingress_for_all_namespaces():
+            # Add only ingresses with pingdom operator annotations
+            for annotation in self.Ingress(ingress).annotations:
+                if annotation.startswith("pingdom-operator.io/"):
+                    ingresses_list.append(self.Ingress(ingress))
+                    break
+
+        return ingresses_list
+
+
+class Pingdom:
+    class BearerAuth(requests.auth.AuthBase):
+        def __init__(self, token):
+            self.token = token
+
+        def __call__(self, r):
+            r.headers["authorization"] = "Bearer " + self.token
+            return r
+
+    def __init__(self, token):
+        self.s = requests.Session()
+        self.s.auth = self.BearerAuth(token)
+        self.s.headers['Content-type'] = 'application/json'
+        self.tags_filter = []
+
+    def api_url(self, *args):
+        url = "https://api.pingdom.com/api/3.1/"
+        args_str = []
+        for arg in args:
+            args_str.append(str(arg))
+        req_url = url + "/".join(args_str)
+        return req_url.rstrip('/')
+
+    # Pingdom supports resolution only one of 1m, 5m, 15m, 30m, 60m check intervals only
+    def __check_interval(self, interval: int = 5):
+        if interval < 1:
+            raise Exception("Interval must be greater than 0")
+        if interval >= 60:
+            return 60
+        if interval >= 30:
+            return 30
+        if interval >= 15:
+            return 15
+        if interval >= 5:
+            return 5
+        if interval >= 1:
+            return 1
+
+    # Enum: "http" "httpcustom" "tcp" "ping" "dns" "udp" "smtp" "pop3" "imap"
+    def __type(self, type):
+        if type not in ["http", "httpcustom", "tcp", "ping", "dns", "udp", "smtp", "pop3", "imap"]:
+            raise Exception("Invalid check type")
+        return type
+
+    def __probe_filters(self, region=None):
+        if region not in [None, 'EU', 'NA', 'APAC', 'LATAM']:
+            raise Exception("Invalid region")
+        return [
+            'region: ' + region
+        ]
+
+    def __parse_headers(self, headers):
         # {
         #     "Date": "Mon, 01 Aug 2022 12:52:45 GMT",
         #     "Content-Type": "application/json",
@@ -46,7 +124,24 @@ class Pingdom():
         #     "CF-RAY": "733eb6789c4268fb-FRA",
         #     "Content-Encoding": "gzip"
         # }
-        self.api_status = dict(response.headers)
+        self.req_limit_short = dict(headers)["req-limit-short"]
+        self.req_limit_long = dict(headers)["req-limit-long"]
+
+    @cached(cache=TTLCache(maxsize=32, ttl=600))
+    def checks(self, *args):
+        response = None
+        url = self.api_url('checks')
+        tags = []
+        params = {}
+        for i in args:
+            tags.append(str(i))
+        if tags:
+            params["tags"] = ','.join(tags)
+            response = self.s.get(url, params=params, json={})
+        else:
+            response = self.s.get(url, json={})
+        response.raise_for_status()
+        self.__parse_headers(response.headers)
 
         # list of
         # {
@@ -67,10 +162,31 @@ class Pingdom():
         # }
         return response.json()['checks']
 
-    @lru_cache(maxsize=64, typed=False)
-    def get_check(self, checkid: int):
-        response = self.s.get(self.api_url + 'checks/' + str(checkid), json={})
-        response.raise_for_status()
+    @cached(cache=TTLCache(maxsize=1024, ttl=600))
+    def describe_check(self, checkid: int = 0, name: str = None, hostname: str = None):
+        response = None
+        checks = []
+        if checkid < 1:
+            checks = self.checks(*self.tags_filter)
+
+        if checkid >= 1:
+            url = self.api_url('checks', checkid)
+            response = self.s.get(url, json={})
+            response.raise_for_status()
+            self.__parse_headers(response.headers)
+
+            if response:
+                return dict(response.json())
+            return None
+
+        if name is not None:
+            for check in checks:
+                if check['name'] == name:
+                    return self.describe_check(checkid=check['id'])
+        elif hostname is not None:
+            for check in checks:
+                if check['hostname'] == hostname:
+                    return self.describe_check(checkid=check['id'])
         # {
         #     "check": {
         #         "id": 11173154,
@@ -110,14 +226,81 @@ class Pingdom():
         #         "probe_filters": []
         #     }
         # }
-        print(json.dumps(dict(response.json()), indent=4))
+
+        # {
+        #     "check": {
+        #         "id": 6126477,
+        #         "name": "example.app/resource/get",
+        #         "resolution": 1,
+        #         "sendnotificationwhendown": 3,
+        #         "notifyagainevery": 0,
+        #         "notifywhenbackup": true,
+        #         "created": 1590669421,
+        #         "type": {
+        #             "http": {
+        #                 "verify_certificate": true,
+        #                 "url": "/resource/get",
+        #                 "encryption": true,
+        #                 "port": 443,
+        #                 "ssl_down_days_before": 7,
+        #                 "postdata": "var=value&var2=value2",
+        #                 "shouldcontain": "\"success\":1",
+        #                 "requestheaders": {
+        #                     "User-Agent": "Pingdom.com_bot_version_1.4_(http://www.pingdom.com/)"
+        #                 }
+        #             }
+        #         },
+        #         "hostname": "example.app",
+        #         "ipv6": false,
+        #         "responsetime_threshold": 30000,
+        #         "custom_message": "",
+        #         "integrationids": [
+        #             103676,
+        #             115348
+        #         ],
+        #         "lasterrortime": 1658593750,
+        #         "lasttesttime": 1659482350,
+        #         "lastresponsetime": 324,
+        #         "lastdownstart": 1658593690,
+        #         "lastdownend": 1658593810,
+        #         "status": "up",
+        #         "tags": [
+        #             {
+        #                 "name": "tag2",
+        #                 "type": "u",
+        #                 "count": "15"
+        #             },
+        #             {
+        #                 "name": "tag1",
+        #                 "type": "u",
+        #                 "count": "4"
+        #             }
+        #         ],
+        #         "probe_filters": [
+        #             "region: EU"
+        #         ]
+        #     }
+        # }
+
 
 
 def main():
-    p = Pingdom()
-    for i in p.checks():
-        print(json.dumps(i, indent=4))
-    # p.get_check(checkid=11173154)
+    token = os.environ.get('BEARER_TOKEN')
+    cluster_name = os.environ.get('CLUSTER_NAME', "default-cluster")
+
+    p = Pingdom(token)
+    p.tags_filter = ['pingdom-operator', cluster_name]
+
+    k = Kubernetes()
+    for ingress in k.pingdom_ingresses():
+        if ingress.annotations.get('pingdom-operator.io/name'):
+            name = ingress.annotations.get('pingdom-operator.io/name')
+            # print(json.dumps(p.describe_check(name=name), indent=2))
+            break
+        for host in ingress.hosts:
+            # print(json.dumps(p.describe_check(hostname=host), indent=2))
+            pass
+
 
 
 main()
